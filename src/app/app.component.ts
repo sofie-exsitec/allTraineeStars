595af43f5901ca6191d338f812b12c9dd2b7c445
Erik Staahl <erik.stahl@exsitec.se>
2020-09-07 14:48:01 +0200

    Fixed label

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'allTraineeStars';
-  webpages = [{ title: 'Simon', url: 'https://simonexsitec.github.io/webtest/' }, {title: 'test', url:"https://simonexsitec.github.io/webtest/"}];
+  webpages = [
+    { title: 'Simon', url: 'https://simonexsitec.github.io/webtest/' },
+    { title: 'test', url: 'https://simonexsitec.github.io/webtest/' },
+  ];
 }

acbd640408d2a054a1987ce616204a969edacf45
Erik Staahl <erik.stahl@exsitec.se>
2020-09-07 16:57:08 +0200

    Populated pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,40 @@ export class AppComponent {
   title = 'allTraineeStars';
   webpages = [
     {
-      title: 'Simon',
-      url: 'https://simonexsitec.github.io/webtest/',
+      title: 'Nicks Sida',
+      url: 'https://pseudonyman.github.io/',
     },
     {
-      title: 'Erik sida',
-      url: 'assets/test.html',
+      title: 'Albins Sida',
+      url: './assets/AlbinsHemsida/index.html',
+    },
+    {
+      title: 'Emelies Sida',
+      url: './assets/EmeliesHemsida/Hemsida.html',
+    },
+    {
+      title: 'Marcus Sida',
+      url: './assets/MarcusHemsida/index.html',
+    },
+    {
+      title: 'Olles Sida',
+      url: './assets/OllesHemsida/index.html',
+    },
+    {
+      title: 'Malins Sida',
+      url: './assets/MalinsHemsida/test.html',
+    },
+    {
+      title: 'Amandas Sida',
+      url: './assets/AmandasHemsida/index.html',
+    },
+    {
+      title: 'Vendelas Sida',
+      url: './assets/VendelasHemsida/index.html',
+    },
+    {
+      title: 'Majas Sida',
+      url: './assets/MajasHemsida/index.html',
     },
   ];
 }
